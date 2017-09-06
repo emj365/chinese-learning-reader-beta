40732b9215c432ae049bc3fca03607effa151963
Michael JIN <emj365@gmail.com>
2017-09-06 16:07:58 +0800

    fix path of texts_data.js

--- js/services.js
+++ js/services.js
@@ -1,59 +1,58 @@
 angular.module('starter.services', [])
 
-.factory('Texts', function($rootScope, $http, $sce) {
-  var getTextsData = function(success, failed) {
+.factory('Texts', function ($rootScope, $http, $sce) {
+  var getTextsData = function (success, failed) {
     if ($rootScope.textsData === undefined) {
-      $http.get('/js/texts_data.js').
-        then(function(response) {
-          $rootScope.textsData = response.data;
-          texts = getTexts(response.data);
-          success(texts);
-        }, function(response) {
-          failed(response.data);
-        });
+      $http.get('js/texts_data.js')
+        .then(function (response) {
+          $rootScope.textsData = response.data
+          texts = getTexts(response.data)
+          success(texts)
+        }, function (response) {
+          failed(response.data)
+        })
     } else {
-      success($rootScope.textsData);
+      success($rootScope.textsData)
     }
-  };
+  }
 
-  var getTexts = function(textsData) {
-    var texts = [];
-    textsData.forEach(function(d) {
-      if (texts.filter(function(t) {
-        return t.textId == d.textId && t.textPart == d.textPart;
+  var getTexts = function (textsData) {
+    var texts = []
+    textsData.forEach(function (d) {
+      if (texts.filter(function (t) {
+        return t.textId == d.textId && t.textPart == d.textPart
       }).length === 0) {
         texts.push({
-          textId:    d.textId,
+          textId: d.textId,
           textTitle: d.textTitle,
-          textPart:  d.textPart,
-        });
+          textPart: d.textPart
+        })
       }
-    });
-    return texts;
-  };
+    })
+    return texts
+  }
 
-  var get = function(textId, textPart, success, failed) {
-    var sentences = $rootScope.textsData.filter(function(text) {
-      return text.textId == textId && text.textPart == textPart;
-    });
-    sentences.map(function(s) {
+  var get = function (textId, textPart, success, failed) {
+    var sentences = $rootScope.textsData.filter(function (text) {
+      return text.textId == textId && text.textPart == textPart
+    })
+    sentences.map(function (s) {
       s.mp3 = '/mp3/' + s.textId + '_' +
-        s.textPart + '_' + s.sentenceId + ".mp3";
-      $sce.trustAsResourceUrl(s.mp3);
-      return s;
-    });
-    success(sentences);
-  };
+        s.textPart + '_' + s.sentenceId + '.mp3'
+      $sce.trustAsResourceUrl(s.mp3)
+      return s
+    })
+    success(sentences)
+  }
 
   return {
-    all: function(success, failed) {
-      getTextsData(success, failed);
+    all: function (success, failed) {
+      getTextsData(success, failed)
     },
-    get: function(textId, textPart, success, failed) {
-      getTextsData(function() {
-        get(textId, textPart, success, failed);
-      }, failed);
+    get: function (textId, textPart, success, failed) {
+      getTextsData(function () {
+        get(textId, textPart, success, failed)
+      }, failed)
     }
-  };
-
-});
+  }
+})
